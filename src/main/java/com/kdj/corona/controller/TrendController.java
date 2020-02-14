e63782f507b4656a0de362f89606152e694d38fe
Kim DongJin <kxv1234@naver.com>
2020-02-14 20:54:53 +0900

    구글 차트 추가

---
+++ src/main/java/com/kdj/corona/controller/TrendController.java
@@ -0,0 +1,110 @@
+package com.kdj.corona.controller;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
+import com.kdj.corona.dto.KeyWord;
+import com.kdj.corona.dto.SearchTrend;
+
+@Controller
+@RequestMapping("trend")
+public class TrendController {
+
+	@RequestMapping("/")
+	public ModelAndView APIExamDatalabTrend(HttpServletRequest request) {
+	
+		String clientId = "aYQR3UekVkUdLCaYXjcF";//애플리케이션 클라이언트 아이디값";
+	    String clientSecret = "ruHZN2NlCd";//애플리케이션 클라이언트 시크릿값";
+        Gson json = new Gson();
+        String answer ="";
+        
+        
+	    try {
+	        String apiURL = "https://openapi.naver.com/v1/datalab/search";
+	        List<String> keywords = new ArrayList<String>();
+	        List<String> keywords2 = new ArrayList<String>();
+	        List<String> keywords3 = new ArrayList<String>();
+	        keywords.add("코로나");
+	        keywords.add("우한");
+	        keywords2.add("마스크");
+	        keywords3.add("확진자");
+	        SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd");
+	        String endDate = format1.format(new Date());
+	        SearchTrend body = SearchTrend.builder()
+	        					.startDate("2020-01-17")
+	        					.endDate(endDate)
+	        					.timeUnit("date")
+	        					.keywordGroups(new KeyWord[] {
+	        							KeyWord.builder()
+	        							.groupName("코로나 바이러스")
+	        							.keywords(keywords)
+	        							.build(),
+	        							KeyWord.builder()
+	        							.groupName("마스크")
+	        							.keywords(keywords2)
+	        							.build(),
+	        							KeyWord.builder()
+	        							.groupName("확진자")
+	        							.keywords(keywords3)
+	        							.build(),
+	        					})
+	        					.device("")
+	        					.ages(new String[] {})
+	        					.gender("")
+	        					.build();
+	        
+	        URL url = new URL(apiURL);
+	        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	        con.setRequestMethod("POST");
+	        con.setRequestProperty("X-Naver-Client-Id", clientId);
+	        con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+	        con.setRequestProperty("Content-Type", "application/json");
+	        
+	
+	        con.setDoOutput(true);
+	        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+	        wr.write(json.toJson(body).getBytes());
+	        wr.flush();
+	        wr.close();
+	
+	        int responseCode = con.getResponseCode();
+	        BufferedReader br;
+	        if(responseCode==200) { // 정상 호출
+	            br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+	        } else {  // 에러 발생
+	            br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"));
+	        }
+	
+	        String inputLine;
+	        StringBuffer response = new StringBuffer();
+	        while ((inputLine = br.readLine()) != null) {
+	            response.append(inputLine);
+	        }
+	        br.close();
+	        System.out.println(response);
+	        answer = response.toString();
+//	        result = json.fromJson(response.toString(),ResponseResult.class);
+	        
+	
+	    } catch (Exception e) {
+	        System.out.println(e);
+	    }
+	   
+	    return new ModelAndView("welcome","result",answer);
+	}
+	
+}
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -5,8 +5,76 @@
 <head>
 <meta charset="EUC-KR">
 <title>Insert title here</title>
+<style type="text/css">
+	  html, body {
+	     position: absolute;
+	     width:100%;
+	     height: 100%;
+	     overflow:hidden;
+	     margin: 0;
+    	 padding: 0;
+	  }
+</style>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 <body>
-"${answer}"
+
+
+<div id="chart_div" style="height : 30%;">
+</div>
+
+<script type="text/javascript">
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+	
+	
+	function drawChart(){
+		
+		var arr = [[],[]];
+		var obj = JSON.parse('${result}');
+		
+		var chart_options = {
+				title : '검색 빈도('+obj.startDate+' ~ '+obj.endDate+')',
+				width : '100%',
+				bar : {
+					groupWidth : '80%' // 예제에서 이 값을 수정
+				},
+				seriesType : 'bars',
+				series : {3 : {type : 'line'}}, // 데이터에서 라인그래프로 만들값을 지정, 3은 순서를 의미하며 0부터 시작
+				isStacked : false // 그래프 쌓기(스택), 기본값은 false
+			};
+		
+		var yAxis = obj.results[0].data.length;
+		var results = obj.results;
+		var date;
+		arr = new Array(yAxis);
+		
+		arr[0] = new Array(results.length+1);
+		arr[0][0] = 'Date';
+		
+		for(var j=1; j<yAxis+1; j++){ //날짜 입력.
+			arr[j] = new Array(results.length+1);
+			date = results[0].data[j-1].period.split('-');
+			arr[j][0] = date[1]+'-'+date[2];
+		}
+		
+		for(var i=0; i<results.length; i++){ // 값 입력.
+			arr[0][i+1] = results[i].title;
+			var data = results[i].data;
+			for(var j=0; j<data.length; j++){
+				arr[j+1][i+1] = data[j].ratio;
+			}
+		}
+		
+		console.log(arr);
+		var data = new google.visualization.arrayToDataTable(arr);
+		var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+		chart.draw(data, chart_options);
+		window.addEventListener('resize', function() { chart.draw(data, chart_options); }, false);
+
+	}
+
+
+</script>
 </body>
 </html>
