968f548c7ed16e7a1c0938838917ef1f996c371e
Kim DongJin <kxv1234@naver.com>
2020-02-18 19:40:43 +0900

    API 아이디, 비밀번호 정보

---
+++ src/main/java/com/kdj/corona/navarAPI/ApiInfo.java
@@ -0,0 +1,7 @@
+package com.kdj.corona.navarAPI;
+
+public class ApiInfo {
+	static String clientId = "aYQR3UekVkUdLCaYXjcF";//애플리케이션 클라이언트 아이디값";
+	static String clientSecret = "ruHZN2NlCd";//애플리케이션 클라이언트 시크릿값";
+
+}
