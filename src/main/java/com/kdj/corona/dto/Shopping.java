3abea7f58292db632a648c218ad5b05982842a54
Kim DongJin <kxv1234@naver.com>
2020-02-18 19:39:29 +0900

    네이버 쇼핑 검색 클래스 생성.

---
+++ src/main/java/com/kdj/corona/dto/Shopping.java
@@ -0,0 +1,21 @@
+package com.kdj.corona.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Shopping {
+	@NonNull private String query;
+	private int display; //기본값 :10, 최댓값: 100
+	private int start; //기본값 :1 , 최댓값:1000
+	private String sort;
+}
