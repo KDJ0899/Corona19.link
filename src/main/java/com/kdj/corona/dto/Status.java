94f2b92c3084dc149c825616671e64fc42d1ab92
Kim DongJin <kxv1234@naver.com>
2020-02-25 20:31:23 +0900

    환자들 현재 상황 클래스 생성.

---
+++ src/main/java/com/kdj/corona/dto/Status.java
@@ -0,0 +1,23 @@
+package com.kdj.corona.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Status {
+	private int quarantinedPatient;
+	private int treatedPatient;
+	private int deceasedPerson;
+	private int inspecting;
+	private String date;
+
+}
