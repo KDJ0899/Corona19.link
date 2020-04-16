269e107d93f1a71483544ddecf1c635b565df857
Kim DongJin <kxv1234@naver.com>
2020-04-16 16:37:56 +0900

    Date, LocalDateTime으로 변경

--- src/main/java/com/kdj/corona/dto/Status.java
+++ src/main/java/com/kdj/corona/dto/Status.java
@@ -1,24 +1,40 @@
 package com.kdj.corona.dto;
 
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Data
+
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Status {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
 	private int id;
+	@Column
 	private int quarantinedPatient;
+	@Column
 	private int treatedPatient;
+	@Column
 	private int deceasedPerson;
+	@Column
 	private int inspecting;
-	private String date;
+	@Column
+	private LocalDateTime date;
 
 }
